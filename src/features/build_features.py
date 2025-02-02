3f99e28bb21cef030796da3044bb6e16f1e35e49
Karim Tache <karim.tache@gmail.com>
2025-02-02 09:47:43 +0100

    add image features script

---
+++ src/features/build_features.py
@@ -0,0 +1,20 @@
+from image_features import extract_image_features
+import pandas as pd
+
+def main():
+
+    # load raw train dataset
+    X_train = pd.read_csv("../../data/raw/X_train.csv", sep=",",index_col=0)
+
+    # Extract text features
+    # todo 
+
+    # Extract image features
+    img_train_rep = "/mnt/c/Users/karim/rakuten/images/data_clean/image_train"
+    X_train = extract_image_features(X_train, img_train_rep)
+
+    # Save processed features
+    X_train.to_csv("../../data/processed/X_train_features.csv", index=False)
+
+if __name__ == "__main__":
+    main()
---
+++ src/features/image_features.py
@@ -0,0 +1,54 @@
+import os
+from PIL import Image, ImageStat
+import imagehash
+
+import pandas as pd
+
+def extract_image_features(df, directory_path):
+    
+    dataset_stats = {
+        'imageid': [],
+        'productid': [],
+        'height': [], 
+        'width': [], 
+        'modes': [], 
+        'formats': [], 
+        'ratios': [], 
+        'file_sizes': [],
+        'mean_luminosity':[],
+        'mean_stddev_luminosity': [],
+        "hash": []
+        }
+    
+    df["filename"] = "image_" + df["imageid"].astype(str) + "_product_" + df["productid"].astype(str) + ".jpg"
+
+    for filename in df["filename"]:
+        try:
+
+            filepath = os.path.join(directory_path, filename)
+            image = Image.open(filepath)
+
+            # extract technical infos about the image
+            dataset_stats['height'].append(image.size[0] )
+            dataset_stats['width'].append(image.size[1])
+            dataset_stats['modes'].append(image.mode)
+            dataset_stats['formats'].append(image.format)
+            dataset_stats['ratios'].append(image.size[0] / image.size[1])
+            dataset_stats['file_sizes'].append(os.path.getsize(filepath))
+            
+            # analyse luminosity
+            image_stat = ImageStat.Stat(image)
+            mean_luminosity = sum(image_stat.mean)/3
+            mean_stddev_luminosity = sum(image_stat.stddev)/3
+            dataset_stats['mean_luminosity'].append(mean_luminosity)
+            dataset_stats['mean_stddev_luminosity'].append(mean_stddev_luminosity)
+            
+            # Generate a perceptual hash to make duplicates analysis easier
+            phash = str(imagehash.phash(image))
+            dataset_stats['hash'].append(phash)
+            
+        except Exception as e:
+            print(f"Erreur lors de l'analyse de {filename}: {str(e)}")
+
+    df_analyzed_img = pd.DataFrame(dataset_stats)       
+    return pd.concat(df, df_analyzed_img)
