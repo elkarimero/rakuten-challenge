b657085edf6477d2535155ce1a76ab6e615f69b1
Karim Tache <karim.tache@gmail.com>
2025-02-09 16:19:55 +0100

    maj nan removal for images

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,21 +1,45 @@
-from image_features import extract_image_features
+from image_features import extract_image_features, prepare_images, remove_na, generate_phash
 import pandas as pd
+import time
 
 def main():
 
     # load raw train dataset
     X_train = pd.read_csv("../../data/raw/X_train.csv", sep=",",index_col=0)
+    Y_train = pd.read_csv("../../data/raw/Y_train.csv", sep=",",index_col=0)
     X_train["filename"] = "image_" + X_train["imageid"].astype(str) + "_product_" + X_train["productid"].astype(str) + ".jpg" # add filename for later processing
+    train_pictures = pd.concat([X_train[["filename"]], Y_train], axis=1)
 
     # Extract text features
     # todo 
 
+    ##############
+    # Images preprocessing
+    ##############
+
+    img_train_rep = "/mnt/c/Users/karim/rakuten/images/data_raw/image_train"
+    #img_train_cleaned_rep = "../../data/processed/image_train"
+    img_train_cleaned_rep = "/mnt/c/Users/karim/rakuten/images/data_clean/image_train"
+
+    # Clean up
+    start = time.time()
+    #prepare_images(train_pictures, img_train_rep, img_train_cleaned_rep)
+    train_pictures = remove_na(train_pictures, img_train_rep)
+    train_pictures.to_csv("../../data/interim/train_pictures.csv")
+    end = time.time()
+    print("temps d'exécution:",end-start)
+
     # Extract image features
-    img_train_rep = "/mnt/c/Users/karim/rakuten/images/data_clean/image_train"
-    X_train = extract_image_features(X_train, img_train_rep)
+    start = time.time()
+    #train_pictures = pd.read_csv("../../data/interim/train_pictures.csv", sep=",",index_col=0)
+    train_pictures["hash"] = train_pictures["filename"].apply(lambda filename: generate_phash(filename,img_train_cleaned_rep))
+    end = time.time()
+    print("temps d'exécution:",end-start) 
 
     # Save processed features
-    X_train.to_csv("../../data/processed/X_train_features.csv", index=False)
+    train_pictures.to_csv("../../data/processed/train_pictures.csv")
+    train_pictures[["filename", "hash"]].to_csv("../../data/processed/X_train_pictures.csv")
+    train_pictures[["prdtypecode"]].to_csv("../../data/processed/Y_train_pictures.csv")
 
 if __name__ == "__main__":
     main()
--- src/features/image_features.py
+++ src/features/image_features.py
@@ -1,10 +1,19 @@
 import os
 import pandas as pd
+import numpy as np
 
 from PIL import Image, ImageStat
 import imagehash
 import cv2
 
+def generate_phash(filename, directory_path):
+    try:
+        filepath = os.path.join(directory_path, filename)
+        image = Image.open(filepath)
+        return str(imagehash.phash(image))
+    except Exception as e:
+        print(f"Erreur lors de l'analyse de {filename}: {str(e)}")
+        return np.nan
 
 def extract_image_features(df, directory_path):
     
@@ -19,8 +28,10 @@ def extract_image_features(df, directory_path):
         'file_sizes': [],
         'mean_luminosity':[],
         'mean_stddev_luminosity': [],
-        "hash": []
+        "hash": [],
+        "mean_std": []
         }
+    data = []
 
     for filename in df["filename"]:
         try:
@@ -29,29 +40,48 @@ def extract_image_features(df, directory_path):
             image = Image.open(filepath)
 
             # extract technical infos about the image
-            dataset_stats['height'].append(image.size[0] )
-            dataset_stats['width'].append(image.size[1])
-            dataset_stats['modes'].append(image.mode)
-            dataset_stats['formats'].append(image.format)
-            dataset_stats['ratios'].append(image.size[0] / image.size[1])
-            dataset_stats['file_sizes'].append(os.path.getsize(filepath))
+            # dataset_stats['height'].append(image.size[0] )
+            # dataset_stats['width'].append(image.size[1])
+            # dataset_stats['modes'].append(image.mode)
+            # dataset_stats['formats'].append(image.format)
+            # dataset_stats['ratios'].append(image.size[0] / image.size[1])
+            # dataset_stats['file_sizes'].append(os.path.getsize(filepath))
             
             # analyse luminosity
-            image_stat = ImageStat.Stat(image)
-            mean_luminosity = sum(image_stat.mean)/3
-            mean_stddev_luminosity = sum(image_stat.stddev)/3
-            dataset_stats['mean_luminosity'].append(mean_luminosity)
-            dataset_stats['mean_stddev_luminosity'].append(mean_stddev_luminosity)
+            #image_stat = ImageStat.Stat(image)
+            #mean_luminosity = sum(image_stat.mean)/3
+            #mean_stddev_luminosity = sum(image_stat.stddev)/3
+            #dataset_stats['mean_luminosity'].append(mean_luminosity)
+            #dataset_stats['mean_stddev_luminosity'].append(mean_stddev_luminosity)
             
             # Generate a perceptual hash to make duplicates analysis easier
             phash = str(imagehash.phash(image))
-            dataset_stats['hash'].append(phash)
+            #dataset_stats['hash'].append(phash)
+
+            # standard deviation analysis
+            #image_array = cv2.imread(filepath)
+            #image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+            #R, G, B = image_array[:, :, 0], image_array[:, :, 1], image_array[:, :, 2]
+            #std_R = np.std(R)
+            #std_G = np.std(G)
+            #std_B = np.std(B)
+            #dataset_stats['mean_std'].append(np.mean([std_R, std_G, std_B]))
             
+            row = {
+                "hash": phash
+            }
+
+            data.append(row)
+
         except Exception as e:
             print(f"Erreur lors de l'analyse de {filename}: {str(e)}")
+            row = {
+                "hash": np.nan
+            }
+            data.append(row)
 
-    df_analyzed_img = pd.DataFrame(dataset_stats)       
-    return pd.concat(df, df_analyzed_img)
+    df_analyzed_img = pd.DataFrame(data)       
+    return df_analyzed_img #pd.concat(df, df_analyzed_img)
 
 
 def prepare_images(df, orig_dir_path, dest_dir_path):
@@ -63,7 +93,7 @@ def prepare_images(df, orig_dir_path, dest_dir_path):
             cv2.imwrite(copy_filepath,img)
             
         except Exception as e:
-            print(f"Erreur lors de l'analyse de {filename}: {str(e)}")
+            print(f"Erreur dans prepare_images pour {filename}: {str(e)}")
 
 
 def cleanup_picture(filepath, show_images = True):
@@ -88,7 +118,7 @@ def cleanup_picture(filepath, show_images = True):
             x_max, y_max, w_max, h_max = x, y, w, h
 
     # Add margin to bounding box 
-    margin = 10
+    margin = 1 
     image_width, image_height = 500, 500
     x = max(0, x_max - margin)
     w = min(w_max + 2 * margin, image_width - x)
@@ -108,3 +138,57 @@ def cleanup_picture(filepath, show_images = True):
     resized = cv2.resize(cropped_image, (new_w, new_h))
 
     return resized
+
+def remove_na(data, src_dir_path):
+
+    # pictures identified as na
+    na_pictures = [
+        'image_1243417369_product_3773721822.jpg',
+        'image_1026011984_product_558486759.jpg', 
+        'image_1190250938_product_2832752183.jpg', 
+        'image_1268286826_product_3948210877.jpg', 
+        'image_1190252023_product_2647272665.jpg', 
+        'image_1261394799_product_3898719785.jpg', 
+        'image_977145542_product_277513729.jpg', 
+        'image_1306155830_product_4164869671.jpg', 
+        'image_1100102350_product_1712289008.jpg'
+        ]
+    
+        # image_1248838417_product_3817897828.jpg
+    
+    # Generate a perceptual hash to identifier similare pictures in the dataset
+    na_hash = []
+    for filename in na_pictures: 
+        filepath = os.path.join(src_dir_path, filename)
+        phash = str(imagehash.phash(Image.open(filepath)))
+        na_hash.append(phash)
+
+    
+    # Generate a perceptual hash for all pictures to identify na in the dataset
+    pictures_hash = []
+    for filename in data["filename"]: 
+        filepath = os.path.join(src_dir_path, filename)
+        phash = str(imagehash.phash(Image.open(filepath)))
+        pictures_hash.append(phash)
+
+    data["hash"] = pictures_hash
+
+    # Remove nan
+    return data[~data["hash"].isin(na_hash)]
+
+
+
+# na ?
+# ffff80aad00a80ab 
+# e66699e1e65a1895 
+# b962df80a20dd4eb
+# bb30f0ca0fcfc270
+# b86bc3903c8f9666
+# d5f5d4926a432cd4
+# ead4956a956a952a
+# eae8953b953f8540
+
+# 87347a3d6bc96485
+# 87347a2f3f8d3085
+
+# e141aeaed134d933
