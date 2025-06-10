c7b91b9d98f74a4fb84a322f68a3579e5f927ed1
= <=>
2025-06-10 22:47:37 +0200

    maj preprocessing script

--- src/features/image_features.py
+++ src/features/image_features.py
@@ -7,6 +7,9 @@ from PIL import Image, ImageStat
 import imagehash
 import cv2
 
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
 def generate_phash(filename, directory_path):
     """
     Génère un hash perceptuel pour une image donnée.
@@ -158,7 +161,6 @@ def zoom_images(df, orig_dir_path, dest_dir_path):
         except Exception as e:
             print(f"Erreur pour {filename}: {str(e)}")
 
-
 def cleanup_picture(filepath, show_images = True):
     """
     Nettoie une image en supprimant le fond blanc et en redimensionnant l'image.
@@ -216,6 +218,7 @@ def cleanup_picture(filepath, show_images = True):
     return resized
 
 def remove_na(data, src_dir_path):
+
     """
     Supprime les images pouvant être considéré comme des NA du DataFrame
     Args:
--- src/features/images_preprocessing.py
+++ src/features/images_preprocessing.py
@@ -1,6 +1,12 @@
 from image_features import extract_image_features, zoom_images, remove_na
+
+# pour le resampling des images
+from images_dataset_resampling import create_balanced_dataset, save_balanced_dataset, copy_images_by_class
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
 import pandas as pd
 import time
+import numpy as np
 
 def main():
 
@@ -10,24 +16,26 @@ def main():
     X_train["filename"] = "image_" + X_train["imageid"].astype(str) + "_product_" + X_train["productid"].astype(str) + ".jpg" # add filename for later processing
     train_pictures = pd.concat([X_train[["filename"]], Y_train], axis=1)
 
-    ##############
-    # Images preprocessing
-    ##############
-
-    #img_train_rep = "../../data/raw/image_train"
-    #img_train_cleaned_rep = "../../data/processed/image_train"
 
     img_train_rep = "/mnt/c/Users/karim/rakuten/images/data_raw/image_train"
     img_train_cleaned_rep = "/mnt/c/Users/karim/rakuten/images/data_clean/image_train"
+    img_tmp = "/mnt/c/Users/karim/rakuten/images/tmp"
+    img_train_cleaned_resampled_rep = "/mnt/c/Users/karim/rakuten/images/data_clean/image_train_resampled"
 
-    # Zoom images
+    ################
+    # Zoom sur l'objet  d'intérêt dans les images
+    ################
+
     print("start: zoom images")
     start = time.time()
     zoom_images(train_pictures, img_train_rep, img_train_cleaned_rep)
     end = time.time()
     print("temps d'exécution:",end-start)
 
-    # Extract image features
+    ################
+    # Extraction des features utiles
+    ################
+
     print("start: Extract image features")
     start = time.time()
     train_pictures = extract_image_features(train_pictures, img_train_cleaned_rep)
@@ -35,11 +43,55 @@ def main():
     end = time.time()
     print("temps d'exécution:",end-start) 
 
+    ################
     # Clean up
+    ################
+
     print("start: Clean up")
     start = time.time()
-    train_pictures = remove_na(train_pictures, img_train_rep)           # suppression des images identifiées comme des valeurs manquantes
-    train_pictures = train_pictures.drop_duplicates(subset=["hash"])    # suppresion des doublons
+
+    # suppression des images identifiées comme des valeurs manquantes
+    train_pictures = remove_na(train_pictures, img_train_rep) 
+
+    # suppresion des doublons          
+    train_pictures = train_pictures.drop_duplicates(subset=["hash"])
+
+    end = time.time()
+    print("temps d'exécution:",end-start)
+
+    ##################
+    # Resampling images
+    ##################
+
+    print("start: Resampling images")
+    start = time.time()
+
+    # Création d'un répertoire temporaire pour les images qui respecte la structure attendue par image_dataset_from_directory
+    copy_images_by_class(train_pictures, img_train_cleaned_rep, img_tmp)
+
+    img_size = (224, 224)  # Taille cible optimisée pour les modèles de vision par ordinateur
+
+    # chargement du dataset d'images
+    ds = image_dataset_from_directory(
+        img_tmp,
+        image_size=img_size,
+        batch_size=128,
+        seed=42
+    )
+
+    # Compter les occurrences de chaque classe
+    class_labels = []
+    for _, labels in ds:
+        class_labels.extend(labels.numpy().tolist())
+    
+    classes, counts = np.unique(class_labels, return_counts=True)
+
+    # Défini la médiane comme seuil pour identifier les classes minoritaires et majoritaires
+    target_samples = threshold = np.median(counts)  
+    # Créer un dataset équilibré de target_samples images par classe
+    balanced_train_ds = create_balanced_dataset(ds.unbatch(), classes, target_samples)
+    # Sauvegarder le dataset équilibré dans un répertoire organisé par classes
+    save_balanced_dataset(balanced_train_ds,img_train_cleaned_resampled_rep, ds.class_names )
     end = time.time()
     print("temps d'exécution:",end-start)
 
