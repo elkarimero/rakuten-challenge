266dce3330243671ccd582ee772f6b287740d738
= <=>
2025-06-30 22:01:00 +0200

    maj mise en forme

--- src/streamlit/pages/problematique.py
+++ src/streamlit/pages/problematique.py
@@ -6,27 +6,34 @@ La classification automatique des produits est un enjeu majeur pour les entrepri
 
 """)
 
-st.header("Enjeux économiques")
+col1, col2 = st.columns(2)
 
-st.markdown("Risques d'une mauvaise classification des produits :")
-st.markdown("- **perte de ventes** en raison d'une mauvaise visibilité des produits")
-st.markdown("- **augmentation des coûts** de mise en ligne et de gestion des stocks en raison d'une mauvaise classification")
-st.markdown("- **impact négatif sur l'expérience utilisateur** et la fidélisation des clients")
+col1.header("Enjeux économiques")
 
-st.markdown("Benefices d'une classification automatisée avec du machine learning:")
-st.markdown("- **accélérer** le temps de mise en ligne des produits")
-st.markdown("- **réduire les liés** à la classification manuelle")
-st.markdown("- améliorer la **précision** de la classification des produits")
-st.markdown("- **expérience utilisateur améliorée** grâce à une recherche de produits plus efficace et de recommendations plus pertinentes")
+col1.markdown("Risques d'une mauvaise classification des produits :")
+col1.markdown("- **Perte de ventes** *en raison d'une mauvaise visibilité des produits*")
+col1.markdown("- **Augmentation des coûts** *de mise en ligne et de gestion des stocks*")
+col1.markdown("- **Impact négatif sur l'expérience utilisateur** *et la fidélisation des clients*")
 
 
-st.header("Enjeux techniques")
-st.markdown("Défis techniques de la classification automatique des produits :")
-st.markdown("- **diversité des données** à analyser : descriptions textuelles, images, etc.")
-st.markdown("- **qualité des données** : catalogue multilingue, qualité des descriptions obtenues par scrapping de site web, doublons, etc.")
-st.markdown("- **besoin de ressources** lié à la taille du dataset d'images à analyser")
+col1.markdown("****")
 
-st.markdown("Compétences mobilisées pour adresser ces défis :")
-st.markdown("- **différentes techniques NLP** comme tokenisation, lemmatisation, filtrage des stop words et vectorisation")
-st.markdown("- **rééquilibrage des datasets** via des méthode de resampling et de data augmentation pour gérer les déséquilibres de classes")
-st.markdown("- **utilisation de techniques avancées** comme le fine-tuning de modèles pré-entraînés (transfert learning)")
+col1.markdown("Benefices d'une classification automatisée avec du machine learning:")
+col1.markdown("- **Accélérer** *le temps de mise en ligne des produits*")
+col1.markdown("- **Réduire les coûts liés** *à la classification manuelle*")
+col1.markdown("- **Améliorer la précision** *de la classification des produits*")
+col1.markdown("- **Expérience utilisateur améliorée** *grâce à une recherche plus efficace et des recommendations plus pertinentes*")
+
+
+col2.header("Enjeux techniques")
+col2.markdown("Défis techniques de la classification automatique des produits :")
+col2.markdown("- **Diversité des données** à analyser : descriptions textuelles, images")
+col2.markdown("- **Qualité des données** : scrapping de site web, multilingue, déséquilibre des classes")
+col2.markdown("- **Besoin de ressources** lié à la taille du dataset d'images à analyser")
+
+col2.markdown("****")
+
+col2.markdown("Compétences mobilisées pour adresser ces défis :")
+col2.markdown("- **Différentes techniques NLP** comme tokenisation, lemmatisation, filtrage des stop words et vectorisation")
+col2.markdown("- **Rééquilibrage des datasets** via des méthode de resampling et de data augmentation pour gérer les déséquilibres de classes")
+col2.markdown("- **Utilisation de techniques avancées** comme le fine-tuning de modèles pré-entraînés (transfert learning)")
