9b50f3f6b0d61eeaf9cffdacf64c0c1ad8d91b2d
JMETHIVIER <julien.methivier.pro@gmail.com>
2025-07-03 15:02:33 +0200

    Add files via upload

--- src/streamlit/pages/preprocessing.py
+++ src/streamlit/pages/preprocessing.py
@@ -1,12 +1,6 @@
 import streamlit as st
 
 st.title("Méthodologie et préparation des données")
-st.write("""
-lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-""")
-
 
 explo_text_tab, explo_image_tab = st.tabs(["Données textuelles", "Images"])
 
