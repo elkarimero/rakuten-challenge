555d4539131a19de3cbd310f51e5d1e701f1e7f6
= <=>
2025-07-03 17:21:19 +0200

    maj demo

--- src/streamlit/pages/demo.py
+++ src/streamlit/pages/demo.py
@@ -54,9 +54,6 @@ def evaluate_fusion(text_model, image_model, X_text, image_paths, y_true, alpha=
     return acc, y_pred
 
 
-
-
-
 st.title("Démonstration interactive")
 st.write("""
 Bienvenue dans notre projet de data science.
@@ -96,34 +93,49 @@ model = load_model()
 
 # Chargement de l'encoder
 label_encoder = joblib.load('./models/label_encoder.joblib')
-text_model = joblib.load('./models/svm2.pkl')
+text_model = joblib.load('./models/inference_pipeline_svc_model.joblib')
 vectorizer = joblib.load("./models/tfidf_vectorizer.pkl")
 
 # Interface Streamlit
-st.title("🖼️ Démonstration - EfficientNetB0 Fine-tuné")
+#st.title("🖼️ Démonstration - EfficientNetB0 Fine-tuné")
 
-uploaded_file   = st.file_uploader("📤 Choisissez une image", type=["jpg", "jpeg", "png"])
-text_input      = st.text_input("📝 Entrez une description du produit")
+form_cols = st.columns(2)
+uploaded_file   = form_cols[0].file_uploader("📤 Choisissez une image", type=["jpg", "jpeg", "png"])
+text_input      = form_cols[1].text_area("📝 Entrez une description du produit")
+
 
 if uploaded_file:
     image = Image.open(uploaded_file).convert("RGB")
 
-    st.image(image, caption="Image chargée", width=100)
+    #form_cols[0].image(image, caption="Image chargée", width=100)
 
-    if st.button("🔍 Prédire"):
-        with st.spinner("Prédiction en cours..."):
+if st.button("🔍 Prédire"):
+    #st.subheader("Performances du modèle avec et sans fine-tuning")
+    fusion = st.columns(1)[0]
+
+    col1, spacer, col2 = st.columns([1, 0.1, 1])
+
+    with st.spinner("Prédiction en cours..."):
+
+        if uploaded_file:
             # Prédiction de l'image
             input_tensor, img_preprocessed = preprocess_image(image)
             predictions = model.predict(input_tensor)[0]
             top_class = np.argmax(predictions)
             confidence = predictions[top_class] * 100
             # Convertir la classe prédite en étiquette
-            predictions = {categories.get(int(label_encoder.inverse_transform([i])[0]), "Inconnu"): float(pred) for i, pred in enumerate(predictions)}
+            img_proba = {categories.get(int(label_encoder.inverse_transform([i])[0]), "Inconnu"): float(pred) for i, pred in enumerate(predictions)}
             top_class = label_encoder.inverse_transform([top_class])[0]  # Décoder la classe prédite
             top_cat = categories.get(int(top_class), "Inconnu")  # Obtenir le nom de la catégorie
 
             # Afficher les résultats
-            cols = st.columns(6)
+            
+            #col1.success(f"✅ Classe prédite : {top_cat} - {top_class} avec une confiance de {confidence:.2f}%")
+            col1.success(f"📸 Classe prédite : {top_cat} - {top_class}  - confiance : {confidence:.2f}%")
+            col1.bar_chart(img_proba, horizontal=True)
+
+            # Afficher le grad-CAM
+            cols = col1.columns(3)
             grad_cam_image, predicted_class, predicted_score = grad_cam(input_tensor, model, model.get_layer("top_conv"))
             # Afficher l'image originale
             cols[0].image(image, caption="Image chargée", width=224)
@@ -131,20 +143,30 @@ if uploaded_file:
             cols[1].image(cv2.cvtColor(img_preprocessed, cv2.COLOR_BGR2RGB), caption="Image traitée", width=224)
             # Afficher l'image avec Grad-CAM
             cols[2].image(grad_cam_image, caption="Image avec Grad-CAM", width=224)
-            st.success(f"✅ Classe prédite : {top_cat} - {top_class} avec une confiance de {confidence:.2f}%")
-            st.bar_chart(predictions)
 
-            # Prédiction du texte
-            if text_input:
-                text_input = [text_input]
-                text_vectorizer = TfidfVectorizer()
-                text_vectorizer.fit(text_input)
-                text_input_vectorized = text_vectorizer.transform(text_input).toarray() 
-                text_predictions = text_model.predict(text_input_vectorized)[0]
-                text_top_class = np.argmax(text_predictions)
-                text_confidence = text_predictions[text_top_class] * 100
-                text_top_class = label_encoder.inverse_transform([text_top_class])[0]  # Décoder la classe prédite
-                text_top_cat = categories.get(int(text_top_class), "Inconnu")
-                st.success(f"✅ Classe prédite pour le texte : {text_top_cat} - {text_top_class} avec une confiance de {text_confidence:.2f}%")
+        # Prédiction du texte
+        if text_input:
+            text_input = [text_input]
+            #text_input_vectorized = vectorizer.transform(text_input).toarray() 
+            text_predictions = text_model.predict_proba(text_input)[0]
+            text_top_class = np.argmax(text_predictions)
+            text_confidence = text_predictions[text_top_class] * 100
+            text_proba = {categories.get(int(label_encoder.inverse_transform([i])[0]), "Inconnu"): float(pred) for i, pred in enumerate(text_predictions)}
+            text_top_class = label_encoder.inverse_transform([text_top_class])[0]  # Décoder la classe prédite
+            text_top_cat = categories.get(int(text_top_class), "Inconnu")
+            #col2.success(f"✅ Classe prédite pour le texte : {text_top_cat} - {text_top_class} avec une confiance de {text_confidence:.2f}%")
+            col2.success(f"📝 Classe prédite : {text_top_cat} - {text_top_class} - confiance : {text_confidence:.2f}%")
+            col2.bar_chart(text_proba, horizontal=True)
 
+        
+        if text_input and uploaded_file:    
+            # Fusion des prédictions
+            alpha = 0.5
+            prob_combined = alpha * predictions + (1 - alpha) * text_predictions
+            predicted_class = np.argmax(prob_combined)
+            combined_confidence = np.max(prob_combined) * 100
+            predicted_class_label = label_encoder.inverse_transform([predicted_class])[0]
+
+            predicted_category = categories.get(int(predicted_class_label), "Inconnu")
+            fusion.success(f"✅ Classe prédite par fusion : {predicted_category} - {predicted_class_label} avec une confiance de {combined_confidence:.2f}%")
 
