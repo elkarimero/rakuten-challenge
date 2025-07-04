8cf5d9afc447a8dbe441f574dc0ae3bbd020e833
= <=>
2025-07-04 17:50:11 +0200

    adjust preprocessing for images

--- src/streamlit/pages/preprocessing.py
+++ src/streamlit/pages/preprocessing.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Méthodologie et préparation des données")
 
-explo_text_tab, explo_image_tab = st.tabs(["Données textuelles", "Préparation des images"])
+explo_text_tab, preprocessing_image_tab, augmentation_image_tag = st.tabs(["Données textuelles", "Preprocessing des images", "Augmentation des images"])
 
 with explo_text_tab:
     with st.expander("**Pipeline du traitement de texte**", expanded=True):
@@ -25,18 +25,20 @@ with explo_text_tab:
         with col3:
             st.image("Plots/Equilibrage_apres.png")    
     
+        
     
-    
-with explo_image_tab:
-    with st.expander("**Centrage et standardisation des images**", expanded=True):
-
-        st.markdown('''
-            - **Nuances de gris :** *On convertit les images en nuances de gris pour réduire la complexité des données et se concentrer sur les contours des objets*
-            - **Binarisation :** *On applique un seuillage pour convertir l'image en noir et blanc, ce qui permet de mieux détecter les contours des objets et de réduire le bruit*     
-            - **Contours :** *On utilise la détection de contours pour identifier les objets dans l'image, ce qui permet de mieux les isoler et de réduire le bruit*
-            - **Bounding box :** *On dessine une boîte englobante autour de l'objet détecté pour mieux visualiser l'objet d'intérêt*
-            - **Zoom :** *On redimensionne l'image pour se concentrer sur l'objet d'intérêt, ce qui permet de mieux le visualiser et de réduire la taille de l'image*
+with preprocessing_image_tab:
+    st.subheader("Pipeline de prétraitement des images")
+    st.markdown("**Objectif :** *Centrer l'image sur l'objet d'intérêt et la redimensionner pour optimiser la performance du modèle de classification*")
+    st.markdown('''
+            1. **Nuances de gris** -> *On convertit les images en nuances de gris pour réduire la complexité des données et se concentrer sur les contours des objets*
+            2. **Binarisation** -> *On applique un seuillage pour convertir l'image en noir et blanc, ce qui permet de mieux détecter les contours des objets et de réduire le bruit*     
+            3. **Contours** -> *On utilise la détection de contours pour identifier les objets dans l'image, ce qui permet de mieux les isoler*
+            4. **Bounding box** -> *On dessine une boîte englobante autour de l'objet détecté pour mieux visualiser l'objet d'intérêt*
+            5. **Zoom** -> *On redimensionne l'image en zoomant sur l'objet d'intérêt, ce qui permet de mieux le visualiser et d'optimiser la taille de l'image pour l'entrainement (224x224)*
         ''')
+    
+    with st.expander("Illustration du pipeline"):
         import cv2
         from preprocessing.image_preprocessing import zoom_picture
 
@@ -50,11 +52,11 @@ with explo_image_tab:
         col6_image.text("Zoom")
 
         img_files = []
-        img_files.append("./images/img0_orig.jpg")
+        #img_files.append("./images/img0_orig.jpg")
         img_files.append("./images/img1_orig.jpg")
         img_files.append("./images/img2_orig.jpg")
         #img_files.append("./images/img3_orig.jpg")
-        img_files.append("./images/small5.jpg")
+        #img_files.append("./images/small5.jpg")
 
         final_images = []
         for i, filepath in enumerate(img_files):
@@ -66,25 +68,30 @@ with explo_image_tab:
             col5_image.image(cv2.cvtColor(rectangle, cv2.COLOR_BGR2RGB), width=200)
             col6_image.image(cv2.cvtColor(resized, cv2.COLOR_BGR2RGB), width=200)
             final_images.append(cv2.cvtColor(resized, cv2.COLOR_BGR2RGB))
-        
-    with st.expander("Rééquilibrage et augmentation des données", expanded=True):
 
-        st.markdown('''
-            - **Rééquilibrage :** *Utilisation de **la médiane** comme seuil d’équilibrage*
-            - **Augmentation :** *Utiliisation de techniques d'augmentation des données pour générer de nouvelles images à partir des images existantes, en appliquant des transformations telles que la rotation, le zoom, le retournement, etc.*
+with augmentation_image_tag:
+    st.subheader("**Rééquilibrage et augmentation des données**")
+    st.markdown('''
+            - **Rééquilibrage** *des classes autour de la* **médiane** 
+                - *suppression des doublons très fréquents dans les classes majoritaires*
+                - *duplication des images de classes minoritaires*
+            - *Génération de nouvelles images à partir des images existantes, en appliquant des transformations telles
+            que* **le retournement horizontal, la rotation, le zoom, le contraste.**
         ''')
+    with st.expander("Exemple d'augmentation des données"):
 
         import streamlit as st
         import tensorflow as tf
         import numpy as np
         from PIL import Image
 
-        flip = st.checkbox("Flip horizontal", value=True)
-        rotation = st.slider("Rotation (± fraction)", 0.0, 0.5, 0.2, step=0.01)
-        zoom = st.slider("Zoom (± fraction)", 0.0, 0.5, 0.2, step=0.01)
-        contrast = st.slider("Contraste (± %)", 0.0, 1.0, 0.2, step=0.01)
+        col_form1, col_form2, col_form3, col_form4  = st.columns([0.7,1.1,1.1,1.1])
+        flip = col_form1.checkbox("Flip horizontal", value=True)
+        rotation = col_form2.slider("Rotation (± fraction)", 0.0, 1.0, 0.2, step=0.1)
+        zoom = col_form3.slider("Zoom (± fraction)", 0.0, 1.0, 0.4, step=0.1)
+        contrast = col_form4.slider("Contraste (± %)", 0.0, 1.0, 0.2, step=0.1)
         
-        for _, image in enumerate(final_images):
+        for _, image in enumerate(final_images[0:1]):
             # Prétraitement
             img = cv2.resize(image, (224, 224))
             img_array = tf.keras.utils.img_to_array(img) / 255.0
