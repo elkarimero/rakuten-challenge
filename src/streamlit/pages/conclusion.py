52925ae2d3c063d5fac2e5fc545b116b3dea9164
= <=>
2025-07-04 08:36:07 +0200

    maj conclusion

--- src/streamlit/pages/conclusion.py
+++ src/streamlit/pages/conclusion.py
@@ -9,21 +9,21 @@ with st.expander("**Le bilan**", expanded=True):
     st.markdown("""
                 - Améliorer la qualité des données (nettoyage, traduction, équilibrage …)
                 - Exploiter efficacement les données textuelles et visuelles (en mettant en place des pipelines complet des deux côtés)
-                - Classer automatiquement les produits Rakuten avec une bonne performance (macro-F1 score > 80%)
+                - Classer automatiquement les produits Rakuten avec une bonne performance (*macro-F1 score > 80%*)
     """)
 
 with st.expander("**Ce que nous avons appris**", expanded=True):
     st.markdown("""
-    - Manipuler des techniques avancées de NLP (traduction automatique, vectorisation TF-IDF et lemmatisation)
-    - Différentes techniques de traitement d'image (data augmentation, détection d'objets)
+    - Manipuler des *techniques avancées de NLP* (traduction automatique, vectorisation TF-IDF et lemmatisation)
+    - Différentes techniques de *traitement d'images* (data augmentation, détection d'objets)
     - Utiliser et fine tuner des modèles pré-entraînés 
     - Fusionner les données textuelles et visuelles pour améliorer la classification
     """)
 
 with st.expander("**Les difficultés rencontrées**", expanded=True):
     st.markdown("""
-    - Complexité du nettoyage textuel (langues multiples, bruit HTML)
-    - Ambiguïté de certaines classes taxonomiques (notamment la classe "jeux société enfants")
+    - Complexité du nettoyage textuel (langues multiples, bruit HTML, caractères spéciaux, problème d'encoding)
+    - Ambiguïté et l'hétérogénéité de certaines classes taxonomiques 
     - La nécessité de gérer deux modalitées qui a demandé une montée en compétence sur un large spectre de modèles et de techniques spécifiques
     - Temps de calcul important pour l'entraînement des modèles, nécessitant une gestion efficace des ressources
     - Fusion des données textuelles et visuelles : montée en compétence sur les modèles de fusion
