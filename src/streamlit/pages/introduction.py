4671df9586f94e6dfef4fc990f2c2339432dcb0d
Geoff-droid <geo.moridin@gmail.com>
2025-07-04 11:38:26 +0200

    mise a jour introduction

--- src/streamlit/pages/introduction.py
+++ src/streamlit/pages/introduction.py
@@ -1,7 +1,29 @@
 import streamlit as st
 
-st.title("Introduction")
+# Introduction
+st.header("Introduction")
 st.write("""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Le sujet de ce projet est une problématique à laquelle la plupart des marketplaces sont confrontées.
+La classification automatique des produits est extrêmement importante pour réduire les coûts,
+améliorer la qualité des données catalogue, et optimiser l’expérience utilisateur.
+Elle permet à une marketplace de proposer des fonctionnalités allant de la recommandation personnalisée
+à la compréhension de requêtes de recherche complexes.
 """)
+
+# Objectifs
+st.header("Objectifs")
+st.write("""
+L’objectif général est de réussir à classer automatiquement des produits dans le catalogue produit de Rakuten France
+en exploitant efficacement les données textuelles et visuelles associées aux produits dans le but d’améliorer la qualité des données,
+optimiser l'expérience utilisateur et réduire les coûts opérationnels.
+""")
+
+# Ajout d'un séparateur pour améliorer la lisibilité
+st.markdown("---")
+
+# Conclusion
+st.markdown("""
+Ce projet a été une trés bonne opportunité pour appliquer des techniques avancées d'apprentissage automatique
+et de traitement de données afin de résoudre un problème réel et impactant dans le domaine du e-commerce.
+""")
+
